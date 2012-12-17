87348804b4ae9dcc59ab5e773b794cb3fb62d073
Matthew Ford <matt@bitzesty.com>
2012-12-17 15:09:19 +0000

    Keep important bot users
    Also removing users with nil permissions

--- db/migrate/20121217131213_remove_old_users.rb
+++ db/migrate/20121217131213_remove_old_users.rb
@@ -1,9 +1,10 @@
 class RemoveOldUsers < ActiveRecord::Migration
   def up
     users_to_keep = Edition.includes(:authors).all.map(&:authors).flatten.uniq
-  
+    users_to_keep << User.find_by_name!("GDS Inside Government Team")
+    users_to_keep << User.find_by_name!("Automatic Data Importer")
     users = User.arel_table
-    users_to_destroy = User.where(users[:uid].eq(nil).or(users[:email].eq(nil))) - users_to_keep
+    users_to_destroy = User.where(users[:uid].eq(nil).or(users[:email].eq(nil)).or(users[:permissions].eq(nil))) - users_to_keep
     users_to_destroy.map(&:destroy)
   end
 
